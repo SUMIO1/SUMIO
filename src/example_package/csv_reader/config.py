5da14df019d5c0f08c8e68e227cb0fea5966523d
Paweł Konop <101901301+marcepanowyy@users.noreply.github.com>
2024-05-10 11:39:23 +0200

    [SIO-29] - Wczytanie i załadowanie zawartości CSV do aplikacji (#6)

---
+++ src/example_package/csv_reader/config.py
@@ -0,0 +1,81 @@
+CONSTRAINTS = {
+    "required_columns": ['name', 'surname', 'age_category', 'age', 'weight_category', 'weight', 'country', 'image_url'],
+    "age_categories": {
+        "Jr. Men": {
+            "min_age": 13,
+            "max_age": 18,
+            "Light-weight": {
+                "min": 0,
+                "max": 80
+            },
+            "Middle-weight": {
+                "min": 80,
+                "max": 100
+            },
+            "Heavy-weight": {
+                "min": 100,
+                "max": float('inf')
+            },
+            "Open-weight": None
+        },
+        "Jr. Women": {
+            "min_age": 13,
+            "max_age": 18,
+            "Light-weight": {
+                "min": 0,
+                "max": 60
+            },
+            "Middle-weight": {
+                "min": 60,
+                "max": 75
+            },
+            "Heavy-weight": {
+                "min": 75,
+                "max": float('inf')
+            },
+            "Open-weight": None
+        },
+        "Men": {
+            "min_age": 19,
+            "max_age": float('inf'),
+            "Light-weight": {
+                "min": 0,
+                "max": 85
+            },
+            "Middle-weight": {
+                "min": 85,
+                "max": 115
+            },
+            "Light Heavyweight": {
+                "min": 100,
+                "max": 115
+            },
+            "Heavy-weight": {
+                "min": 115,
+                "max": float('inf')
+            },
+            "Open-weight": None
+        },
+        "Women": {
+            "min_age": 19,
+            "max_age": float('inf'),
+            "Light-weight": {
+                "min": 0,
+                "max": 65
+            },
+            "Middle-weight": {
+                "min": 65,
+                "max": 73
+            },
+            "Light Heavyweight": {
+                "min": 73,
+                "max": 80
+            },
+            "Heavy-weight": {
+                "min": 80,
+                "max": float('inf')
+            },
+            "Open-weight": None
+        }
+    }
+}
---
+++ src/example_package/csv_reader/csv_reader.py
@@ -0,0 +1,51 @@
+from tkinter import filedialog, messagebox
+from config import CONSTRAINTS as constraints
+import pandas as pd
+
+df = None
+
+
+def validateCSV(df):
+
+    # Check if all required columns are present
+    if not all(col in df.columns for col in constraints['required_columns']):
+        raise ValueError(f"Columns {constraints['required_columns']} are missing in the CSV file.")
+
+    for index, row in df.iterrows():
+
+        # Check if age category is valid
+        if row['age_category'] not in constraints['age_categories']:
+            raise ValueError(f"Invalid age category {row['age_category']} at row {index + 1}")
+
+        # Check if weight category is valid
+        if row['weight_category'] not in constraints['age_categories'][row['age_category']]:
+            raise ValueError(f"Invalid weight category {row['weight_category']} at row {index + 1}")
+
+        # Check if weight is within the valid range
+        weight = row['weight']
+        min_weight = constraints['age_categories'][row['age_category']][row['weight_category']]['min']
+        max_weight = constraints['age_categories'][row['age_category']][row['weight_category']]['max']
+        if not (min_weight <= weight <= max_weight):
+            raise ValueError(f"Weight {weight} is not within the valid range at row {index + 1}")
+
+
+def processCSV(df):
+    print(df.head())
+
+
+def readCSV():
+    file_path = filedialog.askopenfilename(filetypes=[('CSV Files', '*.csv')])
+    if not file_path or not file_path.endswith('.csv'):
+        raise ValueError("Please select a CSV file.")
+    try:
+        global df
+        df = pd.read_csv(file_path)
+        validateCSV(df)
+        processCSV(df)
+
+    except ValueError as e:
+        messagebox.showerror("Error", str(e))
+
+
+if __name__ == '__main__':
+    readCSV()
