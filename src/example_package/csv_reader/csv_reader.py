cbe2d706173b869e53b8169b2b8f315da81fa71a
Paweł Konop <101901301+marcepanowyy@users.noreply.github.com>
2024-05-11 14:36:01 +0200

    [SIO-30] - Graficzna prezentacja załadowanych danych (#7)

--- src/example_package/csv_reader/csv_reader.py
+++ src/example_package/csv_reader/csv_reader.py
@@ -1,48 +1,48 @@
+from src.config.constraints import CONSTRAINTS
 from tkinter import filedialog, messagebox
-from config import CONSTRAINTS as constraints
 import pandas as pd
+from pandas import DataFrame
 
 df = None
 
 
-def validateCSV(df):
+def validateCSV(df: DataFrame):
 
     # Check if all required columns are present
-    if not all(col in df.columns for col in constraints['required_columns']):
-        raise ValueError(f"Columns {constraints['required_columns']} are missing in the CSV file.")
+    if not all(col in df.columns for col in CONSTRAINTS['required_columns']):
+        raise ValueError(f"Columns {CONSTRAINTS['required_columns']} are missing in the CSV file.")
 
     for index, row in df.iterrows():
 
         # Check if age category is valid
-        if row['age_category'] not in constraints['age_categories']:
+        if row['age_category'] not in CONSTRAINTS['age_categories']:
             raise ValueError(f"Invalid age category {row['age_category']} at row {index + 1}")
 
         # Check if weight category is valid
-        if row['weight_category'] not in constraints['age_categories'][row['age_category']]:
+        if row['weight_category'] not in CONSTRAINTS['age_categories'][row['age_category']]:
             raise ValueError(f"Invalid weight category {row['weight_category']} at row {index + 1}")
 
         # Check if weight is within the valid range
         weight = row['weight']
-        min_weight = constraints['age_categories'][row['age_category']][row['weight_category']]['min']
-        max_weight = constraints['age_categories'][row['age_category']][row['weight_category']]['max']
+        min_weight = CONSTRAINTS['age_categories'][row['age_category']][row['weight_category']]['min']
+        max_weight = CONSTRAINTS['age_categories'][row['age_category']][row['weight_category']]['max']
         if not (min_weight <= weight <= max_weight):
             raise ValueError(f"Weight {weight} is not within the valid range at row {index + 1}")
 
 
-def processCSV(df):
+def processCSV(df: DataFrame):
     print(df.head())
 
 
 def readCSV():
     file_path = filedialog.askopenfilename(filetypes=[('CSV Files', '*.csv')])
     if not file_path or not file_path.endswith('.csv'):
-        raise ValueError("Please select a CSV file.")
+        messagebox.showerror("Error", "Please select a CSV file.")
+        return
     try:
         global df
         df = pd.read_csv(file_path)
         validateCSV(df)
-        processCSV(df)
-
     except ValueError as e:
         messagebox.showerror("Error", str(e))
 
--- src/example_package/gui/app.py
+++ src/example_package/gui/app.py
@@ -1,10 +1,17 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from tkinter import messagebox
+from src.example_package.csv_reader import csv_reader
+from src.config.constraints import CONSTRAINTS
 
 
 class SumioApp(App):
-    kv_file = './kivy/app.kv'
+    kv_file = 'kivy/app.kv'
 
     def build(self):
         return MainScreen()
@@ -22,9 +29,12 @@ class MainScreen(BoxLayout):
         if item == 'Quick Start':
             content.add_widget(QuickStart())
         elif item == 'Load CSV file':
-            content.add_widget(LoadCSV())
+            csv_reader.readCSV()
         elif item == 'Show participants':
-            content.add_widget(ShowParticipants())
+            if csv_reader.df is None:
+                messagebox.showerror("Error", "No data to show. Please load a CSV file.")
+                return
+            content.add_widget(ShowParticipants(csv_reader.df))
         elif item == 'Bracket':
             content.add_widget(Bracket())
 
@@ -58,8 +68,38 @@ class LoadCSV(BoxLayout):
     pass
 
 
-class ShowParticipants(BoxLayout):
-    pass
+# name,surname,age_category,age,weight_category,weight,country,image_url
+
+class ShowParticipants(ScrollView):
+
+    def __init__(self, participants_data, **kwargs):
+        super(ShowParticipants, self).__init__(**kwargs)
+        self.participants_data = participants_data
+        self.generate_layout()
+
+    def generate_layout(self):
+
+        layout = GridLayout(cols=7, spacing=26, size_hint_y=None, padding=[dp(20), dp(20)])
+        layout.bind(minimum_height=layout.setter('height'))
+
+        headers = [header.replace('_', ' ').title() for header in CONSTRAINTS["required_columns"][:-1]]
+
+        for header in headers:
+            layout.add_widget(Label(text=header, bold=True, font_size=14, color=(0.1294, 0.1294, 0.1294, 1)))
+
+        for index, participant in self.participants_data.iterrows():
+            self.add_participant_labels(layout, participant)
+
+        self.add_widget(layout)
+
+    def add_participant_labels(self, layout, participant):
+        for label_name in self.participants_data.columns[:-1]:
+            layout.add_widget(Label(text=str(participant[label_name]), font_size=12, color=(0.1294, 0.1294, 0.1294, 1)))
+
+    def print_participant(self, instance):
+        index = instance.parent.children.index(instance)
+        participant = self.participants_data.iloc[index // 8]
+        print(participant)
 
 
 class Bracket(BoxLayout):
